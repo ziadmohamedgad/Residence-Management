7494670a55077debb405165482c10566c4886062
Ziad Gad <ziadmohamedgad@gmail.com>
2025-08-05 02:32:17 +0300

    adding and implementing clsPerson at business layer

---
+++ Business-Layer/clsPerson.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Data_Layer;
+namespace Business_Layer
+{
+    public class clsPerson
+    {
+        public enum enMode { AddNew = 0, Update = 1 }
+        public enMode Mode = enMode.AddNew;
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + SecondName + " " + ThirdName + " " + LastName;
+            }
+        }
+        public string Phone { get; set; }
+        public clsPerson()
+        {
+            this.PersonID = -1;
+            this.FirstName = "";
+            this.SecondName = "";
+            this.ThirdName = "";
+            this.LastName = "";
+            this.Phone = "";
+            this.Mode = enMode.AddNew;
+        }
+        public clsPerson(int PersonID, string FirstName, string SecondName, string ThirdName, string LastName, string Phone)
+        {
+            this.PersonID = PersonID;
+            this.FirstName = FirstName;
+            this.SecondName = SecondName;
+            this.ThirdName = ThirdName;
+            this.LastName = LastName;
+            this.Phone = Phone;
+            this.Mode = enMode.Update;
+        }
+        private bool _AddNewPerson()
+        {
+            this.PersonID = clsPersonData.AddNewPerson(this.FirstName, this.SecondName, this.ThirdName, this.LastName, this.Phone);
+            return (this.PersonID != -1);
+        }
+        private bool _UpdatePerson()
+        {
+            return clsPersonData.UpdatePerson(this.PersonID, this.FirstName, this.SecondName, this.ThirdName, this.LastName, this.Phone);
+        }
+        public static clsPerson Find(int PersonID)
+        {
+            string FirstName = "", SecondName = "", ThirdName = "", LastName = "", Phone = "";
+            bool IsFound = clsPersonData.GetPersonInfoByID(PersonID, ref FirstName, ref SecondName, ref ThirdName, ref LastName, ref Phone);
+            if (IsFound)
+                return new clsPerson(PersonID, FirstName, SecondName, ThirdName, LastName, Phone);
+            else
+                return null;
+        }
+        public bool Save()
+        {
+            switch(this.Mode)
+            {
+                case enMode.AddNew:
+                    {
+                        if (_AddNewPerson())
+                        {
+                            this.Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                            return false;
+                    }
+                case enMode.Update:
+                    return _UpdatePerson();
+            }
+            return false;
+        }
+        public static bool Delete(int PersonID)
+        {
+            return clsPersonData.DeletePerson(PersonID);
+        }
+        public static bool IsPersonExist(int PersonID)
+        {
+            return clsPersonData.IsPersonExist(PersonID);
+        }
+        public static DataTable GetAllPeople()
+        {
+            return clsPersonData.GetAllPeople();
+        }
+    }
+}
